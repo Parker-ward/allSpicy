eef2d3707da2b4aaffe67082d40aae16b7d14f37
Parker <ward.parker26@gmail.com>
2023-03-22 16:52:18 -0600

    yeah yeah

--- allSpicy/Models/Recipe.cs
+++ allSpicy/Models/Recipe.cs
@@ -6,7 +6,8 @@ namespace allSpicy.Models
     public string CreatorId { get; set; }
     public string Title { get; set; }
     public string Category { get; set; }
-    public string instructions { get; set; }
+    public string Instructions { get; set; }
     public string ImgUrl { get; set; }
+    public Profile Creator { get; set; }
   }
 }
